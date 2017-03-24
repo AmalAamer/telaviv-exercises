8f4d92b9ec1eb008a1cd995028bb2e583910b846
AmalAamer <amal.m.s.a97@gmail.com>
2017-03-24 23:18:57 +0300

    Upload exercise 2.

---
+++ Exercise 2 - 316066497.py
@@ -0,0 +1,59 @@
+''' Exercise #2. Python for Engineers.'''
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+numbers = ??? # Replace ??? with a list of positive numbers (int/float).
+
+# Write a code with for loop below here.
+
+
+# Write a code with while loop below here.
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+A = ??? # Replace ??? with a list of numbers (int/float).
+
+# Write the rest of the code for question 2 below here.
+
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+B = ??? # Replace ??? with a list of numbers (int/float).
+
+# Write the code for question 3 below here.
+
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+str_lst = ??? # Replace ??? with a list of strings.
+
+# Write the code for question 4 below here.
+
+    
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+rep_str = ??? # Replace ??? with a string of your choice.
+k = ??? # Replace ??? with a positive int of your choice.
+
+# Write the code for question 5 below here.
+
+
+
+#########################################
+# Question 6 - do not delete this comment
+#########################################
+mat = ??? # Replace ??? with a list of lists of numbers (int/float).
+
+# Write the code for question 6 below here.
+
+
+
