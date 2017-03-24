ab14f044bbbde2b8e532f0061117bf226f66c6b0
AmalAamer <amal.m.s.a97@gmail.com>
2017-03-24 23:56:06 +0300

    Updated ex2

--- Exercise 2 - 316066497.py
+++ Exercise 2 - 316066497.py
@@ -3,18 +3,26 @@
 #########################################
 # Question 1 - do not delete this comment
 #########################################
-numbers = ??? # Replace ??? with a list of positive numbers (int/float).
+# Replace ??? with a list of positive numbers (int/float).
+array = [10,6,9,7,8] 
+i = 0
+arrayLength = len(array) - 1
+result = array[0]
 
-# Write a code with for loop below here.
-
-
-# Write a code with while loop below here.
-
+while (i < arrayLength):
+    if (array[i] >= result):
+        result = array[i]
+    i = i + 1
+print result
+      
+numbers = array  
+#for num in numbers:
+    
 
 #########################################
 # Question 2 - do not delete this comment
 #########################################
-A = ??? # Replace ??? with a list of numbers (int/float).
+#A = ??? # Replace ??? with a list of numbers (int/float).
 
 # Write the rest of the code for question 2 below here.
 
@@ -23,7 +31,7 @@ A = ??? # Replace ??? with a list of numbers (int/float).
 #########################################
 # Question 3 - do not delete this comment
 #########################################
-B = ??? # Replace ??? with a list of numbers (int/float).
+#B = ??? # Replace ??? with a list of numbers (int/float).
 
 # Write the code for question 3 below here.
 
@@ -32,7 +40,7 @@ B = ??? # Replace ??? with a list of numbers (int/float).
 #########################################
 # Question 4 - do not delete this comment
 #########################################
-str_lst = ??? # Replace ??? with a list of strings.
+#str_lst = ??? # Replace ??? with a list of strings.
 
 # Write the code for question 4 below here.
 
@@ -41,8 +49,8 @@ str_lst = ??? # Replace ??? with a list of strings.
 #########################################
 # Question 5 - do not delete this comment
 #########################################
-rep_str = ??? # Replace ??? with a string of your choice.
-k = ??? # Replace ??? with a positive int of your choice.
+#rep_str = ??? # Replace ??? with a string of your choice.
+#k = ??? # Replace ??? with a positive int of your choice.
 
 # Write the code for question 5 below here.
 
@@ -51,7 +59,7 @@ k = ??? # Replace ??? with a positive int of your choice.
 #########################################
 # Question 6 - do not delete this comment
 #########################################
-mat = ??? # Replace ??? with a list of lists of numbers (int/float).
+#mat = ??? # Replace ??? with a list of lists of numbers (int/float).
 
 # Write the code for question 6 below here.
 
